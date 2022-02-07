ede6c68d12c016117e1da78bd979aa6d40e52026
Evgenij Kobzev <you@gmail.com>
2022-02-07 23:15:17 +0300

    change port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+// app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
