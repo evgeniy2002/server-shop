f791c0d24f60b96655208418fa4931da1df4f24f
Evgenij Kobzev <you@gmail.com>
2022-02-08 01:05:27 +0300

    new commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const fileUpload = require('express-fileupload')
 // const { Client } = require('pg');
 
-const PORT = process.env.PORT || '5000'
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
