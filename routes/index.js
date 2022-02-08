c0830e2032ce3fe9c932df84451e63395a9c31fd
Evgenij Kobzev <you@gmail.com>
2022-02-08 12:04:58 +0300

    new change-7

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-// const Router = require('express')
-// const router = new Router()
-// const deviceRouter = require('./deviceRouter')
-// const typeRouter = require('./typeRouter')
-// const searchRouter = require('./searchRouter')
-// const brandRouter = require('./brandRouter')
-// const authRouter = require('./authRouter')
+const Router = require('express')
+const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const typeRouter = require('./typeRouter')
+const searchRouter = require('./searchRouter')
+const brandRouter = require('./brandRouter')
+const authRouter = require('./authRouter')
 
-// router.use('/type', typeRouter)
-// router.use('/device', deviceRouter)
-// router.use('/search', searchRouter)
-// router.use('/brand', brandRouter)
-// router.use('/admin', authRouter)
+router.use('/type', typeRouter)
+router.use('/device', deviceRouter)
+router.use('/search', searchRouter)
+router.use('/brand', brandRouter)
+router.use('/admin', authRouter)
 
-// module.exports = router
+module.exports = router
