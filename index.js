a136c75e80bf6eb8e7b2784dcc323d46cdd2214f
Evgenij Kobzev <you@gmail.com>
2022-02-08 11:13:40 +0300

    new change-3

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-// const router = require('./routes/index')
 const app = express()
+const router = require('./routes/index')
 const fileUpload = require('express-fileupload')
 
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))
-// app.use('/api', router)
+app.use('/api', router)
+app.use(app.router)
 
 
 let PORT = process.env.PORT || 8080
