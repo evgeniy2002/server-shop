76fb0b2ab44649cc65fe85963d04a7869c8f09aa
Evgenij Kobzev <you@gmail.com>
2022-02-08 10:59:29 +0300

    new change-1

--- index.js
+++ index.js
@@ -4,40 +4,18 @@ const cors = require('cors')
 const router = require('./routes/index')
 const app = express()
 const fileUpload = require('express-fileupload')
-// const { Client } = require('pg');
-
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
 
 let PORT = process.env.PORT || 8080
 
+console.log(process.env.PORT)
 
 app.listen(PORT, (req, res) => console.log('server been started on ' + PORT))
-
-// app.get('/', (req, res) => {
-//   res.status(200).json('Hello server')
-// })
-
-  
-
-// app.post("/upload", function (req, res) {
-
-
-//   if (!req.files) {
-//     res.send("File was not found");
-//     return;
-//   }
-
-
-//   res.send("File Uploaded");
-
-
-// });
 
 
 
