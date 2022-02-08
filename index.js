46cb8964c8553b6874bc21a662b9495962595db6
Evgenij Kobzev <you@gmail.com>
2022-02-08 15:19:41 +0300

    deploy-5

--- index.js
+++ index.js
@@ -2,31 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-// const router = require('./routes/index.js')
 const fileUpload = require('express-fileupload')
+const router = require('./routes/index.js')
 const path = require('path')
-// const searchRouter = require('./routes/searchRouter')
-// const typeRouter = require('./routes/typeRouter')
-// const deviceRouter = require('./routes/deviceRouter')
-// const brandRouter = require('./routes/brandRouter')
-// const authRouter = require('./routes/authRouter')
-
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
-// app.use('/api', router)
-
-// app.use('/api/type', typeRouter)
-// app.use('/api/device', deviceRouter)
-// app.use('/api/search', searchRouter)
-// app.use('/api/brand', brandRouter)
-// app.use('/api/admin', authRouter)
-
-// router.get("/todos", function(req, res) {
-//   res.json(todos);
-// });
+app.use('/api', router)
 
 
 let PORT = process.env.PORT || 8080
@@ -35,7 +19,50 @@ app.get('/', (req,res) => {
   res.status(200).json('hello')
 })
 
-app.listen(PORT, (req, res) => console.log('server been started on ' + PORT))
+app.listen(PORT, (req, res) => {})
+
+
+
+
+
+// require('dotenv').config()
+// const express = require('express')
+// const cors = require('cors')
+// const app = express()
+// // const router = require('./routes/index.js')
+// const fileUpload = require('express-fileupload')
+// const path = require('path')
+// // const searchRouter = require('./routes/searchRouter')
+// // const typeRouter = require('./routes/typeRouter')
+// // const deviceRouter = require('./routes/deviceRouter')
+// // const brandRouter = require('./routes/brandRouter')
+// // const authRouter = require('./routes/authRouter')
+
+
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.static(path.resolve(__dirname, 'static')))
+// app.use(fileUpload({}))
+// // app.use('/api', router)
+
+// // app.use('/api/type', typeRouter)
+// // app.use('/api/device', deviceRouter)
+// // app.use('/api/search', searchRouter)
+// // app.use('/api/brand', brandRouter)
+// // app.use('/api/admin', authRouter)
+
+// // router.get("/todos", function(req, res) {
+// //   res.json(todos);
+// // });
+
+
+// let PORT = process.env.PORT || 8080
+
+// app.get('/', (req,res) => {
+//   res.status(200).json('hello')
+// })
+
+// app.listen(PORT, (req, res) => console.log('server been started on ' + PORT))
 
 
 
