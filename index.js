218de158307268739d93bbda826f9d01ba65e8e7
Evgenij Kobzev <you@gmail.com>
2022-02-08 15:28:19 +0300

    deploy-6

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const fileUpload = require('express-fileupload')
-const router = require('./routes/index.js')
+const searchRouter = require('./routes/searchRouter')
+const typeRouter = require('./routes/typeRouter')
+const deviceRouter = require('./routes/deviceRouter')
+const brandRouter = require('./routes/brandRouter')
+const authRouter = require('./routes/authRouter')
 const path = require('path')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
-app.use('/api', router)
+app.use('/api/type', typeRouter)
+app.use('/api/device', deviceRouter)
+app.use('/api/search', searchRouter)
+app.use('/api/brand', brandRouter)
+app.use('/api/admin', authRouter)
 
 
 let PORT = process.env.PORT || 8080
 
-app.get('/', (req,res) => {
-  res.status(200).json('hello')
-})
-
-app.listen(PORT, (req, res) => {})
+app.listen(PORT, (req, res) => console.log('server been started on ' + PORT))
 
 
 
@@ -32,11 +36,11 @@ app.listen(PORT, (req, res) => {})
 // // const router = require('./routes/index.js')
 // const fileUpload = require('express-fileupload')
 // const path = require('path')
-// // const searchRouter = require('./routes/searchRouter')
-// // const typeRouter = require('./routes/typeRouter')
-// // const deviceRouter = require('./routes/deviceRouter')
-// // const brandRouter = require('./routes/brandRouter')
-// // const authRouter = require('./routes/authRouter')
+// const searchRouter = require('./routes/searchRouter')
+// const typeRouter = require('./routes/typeRouter')
+// const deviceRouter = require('./routes/deviceRouter')
+// const brandRouter = require('./routes/brandRouter')
+// const authRouter = require('./routes/authRouter')
 
 
 // app.use(cors())
@@ -45,11 +49,11 @@ app.listen(PORT, (req, res) => {})
 // app.use(fileUpload({}))
 // // app.use('/api', router)
 
-// // app.use('/api/type', typeRouter)
-// // app.use('/api/device', deviceRouter)
-// // app.use('/api/search', searchRouter)
-// // app.use('/api/brand', brandRouter)
-// // app.use('/api/admin', authRouter)
+// app.use('/api/type', typeRouter)
+// app.use('/api/device', deviceRouter)
+// app.use('/api/search', searchRouter)
+// app.use('/api/brand', brandRouter)
+// app.use('/api/admin', authRouter)
 
 // // router.get("/todos", function(req, res) {
 // //   res.json(todos);
