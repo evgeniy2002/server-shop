ad33621297e16687b14cf88dd0ebe5f7bf124361
Evgenij Kobzev <you@gmail.com>
2022-02-08 11:08:47 +0300

    new change-2

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const router = require('./routes/index')
+// const router = require('./routes/index')
 const app = express()
 const fileUpload = require('express-fileupload')
 
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))
-app.use('/api', router)
+// app.use('/api', router)
 
 
 let PORT = process.env.PORT || 8080
 
-console.log(process.env.PORT)
+app.get('/', (req, res) => res.status(200).json('hello'))
 
 app.listen(PORT, (req, res) => console.log('server been started on ' + PORT))
 
