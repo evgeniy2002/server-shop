9d3ee0962607c370a6e3d8afecd4caf7dd5e0f5a
Evgenij Kobzev <you@gmail.com>
2022-03-12 00:06:05 +0300

    new server v5.0.0

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const authController = require('../controllers/authController')
 
-// router.post('/regist', authController.registration)
+router.post('/regist', authController.registration)
 router.post('/login', authController.login)
 
 module.exports = router;
