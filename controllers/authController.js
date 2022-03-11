2b40055001c122f56e820fdf88d7f37db8234b5f
Evgenij Kobzev <you@gmail.com>
2022-03-12 00:14:51 +0300

    new server v5.0.0(1)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,27 @@
 const db = require('../db')
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
 class AuthController {
-  async registration(req, res) {
-    try {
-      const { name, password } = req.body
+  // async registration(req, res) {
+  //   try {
+  //     const { name, password } = req.body
 
-      const hashPassword = bcrypt.hashSync(password, 7)
+  //     const hashPassword = bcrypt.hashSync(password, 7)
 
-      const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
+  //     const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
 
-      res.json(new_user.rows[0])
-      return res.json({ message: 'Пользователь успешно зарегестрирован' })
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: 'registration error' })
-    }
-  }
+  //     res.json(new_user.rows[0])
+  //     return res.json({ message: 'Пользователь успешно зарегестрирован' })
+  //   } catch (e) {
+  //     console.log(e)
+  //     res.status(400).json({ message: 'registration error' })
+  //   }
+  // }
   async login(req, res) {
     try {
       const { login, password } = req.body
-      // console.log(req.body)
+
       const admin_password = await db.query('select password from admin_user where user_name = $1', [login])
       const admin_id = await db.query('select id from admin_user where user_name = $1', [login])
   
@@ -30,7 +30,6 @@ class AuthController {
       if(!validPassword){
         return res.status(400).json({message: 'invalid password'})
       }
-      console.log(admin_id.rows.length)
      if(admin_id.rows.length){
        const token = jwt.sign({ id: admin_id }, process.env.JWT_SECRET_KEY, { expiresIn: '24h' })
        return res.status(200).json({ token: token })
