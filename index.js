87dfdd74c39afea6b157dbfa6d3036cdec777e1e
Evgenij Kobzev <you@gmail.com>
2022-03-12 12:30:54 +0300

    new server v5.0.0(11)

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.use('/api/brand', brandRouter)
 app.use('/api/search', searchRouter)
 app.use('/api/admin', authRouter)
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 
 let PORT = process.env.PORT || 8080
 
