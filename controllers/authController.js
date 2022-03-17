76c1e46787cc7666580afca1a316c620b046e366
Evgenij Kobzev <you@gmail.com>
2022-03-17 15:16:11 +0300

    new server v7.0.0(1)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,23 @@
 const db = require('../db')
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
 class AuthController {
-  async registration(req, res) {
-    try {
-      const { name, password } = req.body
+  // async registration(req, res) {
+  //   try {
+  //     const { name, password } = req.body
 
-      const hashPassword = bcrypt.hashSync(password, 7)
+  //     const hashPassword = bcrypt.hashSync(password, 7)
 
-      const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
+  //     const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
 
-      res.json(new_user.rows[0])
-      return res.json({ message: 'Пользователь успешно зарегестрирован' })
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: 'registration error' })
-    }
-  }
+  //     res.json(new_user.rows[0])
+  //     return res.json({ message: 'Пользователь успешно зарегестрирован' })
+  //   } catch (e) {
+  //     console.log(e)
+  //     res.status(400).json({ message: 'registration error' })
+  //   }
+  // }
   async login(req, res) {
     try {
       const { login, password } = req.body
