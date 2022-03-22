01277d84d3feb076a0ac191cb4fae633955b6931
Evgenij Kobzev <you@gmail.com>
2022-03-22 20:56:42 +0300

    new server v10.0.0(21)

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(fileUpload({}))
 
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://murmuring-beyond-94675.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
