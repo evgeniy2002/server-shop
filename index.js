849956bde342df1c4c84f041aeb2ceddf833e67d
Evgenij Kobzev <you@gmail.com>
2022-03-22 21:03:33 +0300

    new server v10.0.0(24)

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
+  
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
