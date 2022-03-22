39956d694116fb9e833780fdfc8b5fd9bd6f479d
Evgenij Kobzev <you@gmail.com>
2022-03-22 22:27:48 +0300

    new server v10.0.0(28)

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.use(cors({
 
 app.use(function (req, res, next) {
 
-  // res.setHeader('Access-Control-Allow-Origin', 'https://murmuring-beyond-94675.herokuapp.com');
   res.setHeader('Access-Control-Allow-Origin', 'https://murmuring-beyond-94675.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
@@ -50,12 +49,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
+
 app.use('/api/type', typeRouter)
 app.use('/api/device',deviceRouter)
 app.use('/api/brand', brandRouter)
