a7464c10092bde7bc9f295c69afdf339fae9fce8
Evgenij Kobzev <you@gmail.com>
2022-03-22 20:51:42 +0300

    new server v10.0.0(19)

--- index.js
+++ index.js
@@ -37,15 +37,15 @@ app.use(cors({
 // app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use('/api/type', typeRouter)
 app.use('/api/device',deviceRouter)
