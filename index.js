462da3e9989ea64c082173a13c8d4ff48206c60b
Evgenij Kobzev <you@gmail.com>
2022-03-23 12:54:12 +0300

    new server v11.0.0(2)

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const authRouter = require('./routes/authRouter')
 const path = require('path')
 const router = require('./routes/index')
 
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(cors())
 app.use(express.json())
 
