b00e324a7eb17799ac4b3f6a15e7fbb1943dc38c
Evgenij Kobzev <you@gmail.com>
2022-03-24 11:44:17 +0300

    new server v12.0.0(5)

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const router = require('./routes/index')
 
 
 const corsOptions = {
-  "origin": false,
+  "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204,
