f7ca3ac49e278c6bf45e94a55d23eb15ac4d73f5
Evgenij Kobzev <you@gmail.com>
2022-04-08 19:07:35 +0300

    store server v1.0.0(3)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,29 @@
 const db = require('../db')
 const jwt = require('jsonwebtoken')
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 class AuthController {
-  // async registration(req, res) {
-  //   try {
-  //     const { name, password } = req.body
+  async registration(req, res) {
+    try {
+      const { name, password } = req.body
 
-  //     const hashPassword = bcrypt.hashSync(password, 7)
+      const hashPassword = bcrypt.hashSync(password, 7)
 
-  //     const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
+      const new_user = await db.query('insert into admin_user( user_name, password ) values($1, $2) returning *', [name, hashPassword])
 
-  //     res.json(new_user.rows[0])
-  //     return res.json({ message: 'Пользователь успешно зарегестрирован' })
-  //   } catch (e) {
-  //     console.log(e)
-  //     res.status(400).json({ message: 'registration error' })
-  //   }
-  // }
+      res.json(new_user.rows[0])
+      return res.json({ message: 'Пользователь успешно зарегестрирован' })
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({ message: 'registration error' })
+    }
+  }
   async login(req, res) {
     try {
       const { login, password } = req.body
 
       const admin_id = await db.query('select id from admin_user where user_name = $1', [login])
-      // const admin_password = await db.query('select password from admin_user where user_name = $1', [login])
+      const admin_password = await db.query('select password from admin_user where user_name = $1', [login])
 
 
       // const matchPassword = async (enteredpassword, oldPassword) => {
@@ -37,11 +37,12 @@ class AuthController {
       // console.log(admin_password.rows)
 
       // console.log(validPassword)
-      // const validPassword = bcrypt.compareSync(password, admin_password.rows[0].password);
+      const validPassword = bcrypt.compareSync(password, admin_password.rows[0].password);
 
-      // if (!validPassword) {
-      //   return res.status(400).json({ message: 'invalid password' })
-      // }
+      if (!validPassword) {
+        return res.status(400).json({ message: 'invalid password' })
+      }
+      
       const token = jwt.sign({ id: admin_id }, process.env.JWT_SECRET_KEY, { expiresIn: '24h' })
       return res.status(200).json({ token: token })
 
