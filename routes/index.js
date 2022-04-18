218ded7f4d47db9ee47d2594859bb812cb5a1b6b
Evgenij Kobzev <you@gmail.com>
2022-04-18 11:13:35 +0300

    new server v5.0.0

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,11 @@ const typeRouter = require('./typeRouter')
 const searchRouter = require('./searchRouter')
 const brandRouter = require('./brandRouter')
 const authRouter = require('./authRouter')
-const infoRouter = require('./infoRouter')
 
 router.use('/type', typeRouter)
 router.use('/device', deviceRouter)
 router.use('/search', searchRouter)
 router.use('/brand', brandRouter)
 router.use('/admin_panel', authRouter)
-router.use('/info', infoRouter)
 
 module.exports = router
